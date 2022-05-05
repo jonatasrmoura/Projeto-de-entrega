e50c65cd9516fdb853bfd08dcbf654c587715fe5
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-05-04 23:24:50 -0300

    feat#Cadastro de deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
-const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createClientController = new CreateClientController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/authenticate", authenticateClientController.handle);
 
 routes.post("/client", createClientController.handle);
+
+routes.post("/deliveryman", createDeliveryController.handle);
 
 export { routes };
