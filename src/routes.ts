8596f7b6bcbae8232c13b2f250d2a8868a37e7d1
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-05-05 23:08:54 -0300

    feat#Busca de entregas sem deliveryman atrelado

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/FindAllAvailable/FindAllAvailableController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -15,16 +16,17 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
-
-routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
-
 routes.post("/client", createClientController.handle);
 
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
 
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle);
+routes.get("/delivery/available", ensureAuthenticateClient, findAllAvailableController.handle);
 
 export { routes };
